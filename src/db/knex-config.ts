ef9e67facc42cec2cb9617ddf3243f8c07850f4c
igorfals <igorfeals@gmail.com>
2024-10-27 15:53:44 -0300

    feat: criando configuração do arquivo knex-config.ts para conectar ao banco

---
+++ src/db/knex-config.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import knex, { Knex } from 'knex';
+dotenv.config();
+
+const knexConfig: Knex.Config = {
+    client: 'mysql',
+    connection: {
+        host: process.env.DB_HOST || 'localhost',
+        port: Number(process.env.DB_PORT) || 3306,
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '3636',
+        database: process.env.DB_NAME || 'database-foodtruck',
+    },
+};
+
+const db: Knex = knex(knexConfig);
+
+export default db;
